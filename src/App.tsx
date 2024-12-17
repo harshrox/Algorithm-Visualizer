d8e613d4c3bfafed0e4b30491a0274bb127e5fca
Harsh <harshjha08@outlook.com>
2024-12-17 21:19:07 +0530

    Tile context completed

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { PathfindingProvider } from './contexts/PathfindingContext'
+import { TileProvider } from './contexts/TileContext'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <PathfindingProvider>
-      <div className='font-mono'>
-        Hello World
-      </div>
+      <TileProvider>
+        <div className='font-mono'>
+          Hello World
+        </div>
+      </TileProvider>
     </PathfindingProvider>
   )
 }
